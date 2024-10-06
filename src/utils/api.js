f31bce57eaddb3aa367a514e5922678854ded3cf
Brandon King <brandonking25@gmail.com>
2024-10-06 14:08:11 -0400

    Updated api url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,7 @@
-export const baseUrl = "http://localhost:3001";
+export const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://api.wtwr.flylnk.com"
+    : "http://localhost:3001";
 /**============================================
  *           Fetch and Check Functions
  *=============================================**/
